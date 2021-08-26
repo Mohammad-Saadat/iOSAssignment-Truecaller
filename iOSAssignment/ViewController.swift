8e9ec1417cc6d259e882c258038a9a365886fcb5
Mohammad Saadat <muhammad.saadaat@gmail.com>
2021-08-27 00:13:38 +0430

    initialize

---
+++ iOSAssignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOSAssignment
+//
+//  Created by mohammadSaadat on 6/5/1400 AP.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
